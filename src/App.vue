4b57ab3ae48370f3163148cc89d780e6133988da
zulemaCoronel <zulema.cl.09@gmail.com>
2024-07-22 15:06:36 -0600

    Formulario y lista reactiva

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 
 <template>
 <Contador></Contador>
+<ListaDeTareas></ListaDeTareas>
 </template>
 
 
 <script setup>
 import Contador from './components/Contador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 
 
 </script>
