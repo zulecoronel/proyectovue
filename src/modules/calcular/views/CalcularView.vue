cba11cd3eab3ea1ec804c67362da259be66a1377
zulemaCoronel <zulema.cl.09@gmail.com>
2024-07-24 10:15:09 -0600

    V-show y v-if para condicionar impresion de elementos

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,7 +2,9 @@
     <h3>Propiedades computadas</h3>
     <input type="text" v-model="valor">
     {{ valorCalculado }}
-    <ResultadoSection :resultado="valorCalculado"></ResultadoSection>
+    <ResultadoSection v-if="mostrarElemento":resultado="valorCalculado"></ResultadoSection>
+    <div v-else-if="isNaN(valor)">Debe ser un numero</div>
+    <div v-else >Aqui no se muestra nada</div>
 
 </template>
     
@@ -14,6 +16,9 @@ import ResultadoSection from '../components/ResultadoSection.vue';
 const valor = ref('');
 const valorCalculado = computed(() => {
     return valor.value * 3;
+})
+const mostrarElemento = computed(()=>{
+    return valor.value >10;
 })
 </script>
 
